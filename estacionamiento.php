f3719153ad287f490b58f38bacdd3c7d42abd13e
matias mirotta <m.mirotta@gmail.com>
2015-09-07 19:53:10 -0300

    nuevos autos

--- estacionamiento.php
+++ estacionamiento.php
@@ -7,7 +7,7 @@
 		public static function Guardar($patente)
 		{
 			$archivo=fopen("autos.txt", "a"); //escribe pero mantiene lo que estaba
-			$ahora = date("Y-m-d H:i:s");
+			$ahora = date("Y-m-d h:i:s");
 			$renglon = $patente . "=>" . $ahora . "\n";
 			fwrite($archivo, $renglon);
 			fclose($archivo);
